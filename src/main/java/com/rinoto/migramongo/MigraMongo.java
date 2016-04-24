77e7af42c6ec5fe93b3fee3db75660ba6f50d468
rinoto <ruben_inoto@yahoo.com>
2016-04-24 16:35:50 +0200

    migEntry

--- src/main/java/com/rinoto/migramongo/MigraMongo.java
+++ src/main/java/com/rinoto/migramongo/MigraMongo.java
@@ -18,152 +18,159 @@ import com.mongodb.client.MongoDatabase;
 
 public class MigraMongo {
 
-    private static final String MIGRAMONGO_COLLECTION = "_migramongo";
-    private final MongoDatabase database;
+	private static final String MIGRAMONGO_COLLECTION = "_migramongo";
+	private final MongoDatabase database;
 
-    @Autowired
-    private ApplicationContext appContext;
+	@Autowired
+	private ApplicationContext appContext;
 
-    public MigraMongo(MongoDatabase database) {
-        this.database = database;
-    }
+	public MigraMongo(MongoDatabase database) {
+		this.database = database;
+	}
 
-    public MigraMongoStatus migrate() {
-        final Document lastMigrationApplied = getLastMigrationApplied();
-        if (lastMigrationApplied == null) {
-            final Object initialMigrationScript = getInitialMigrationScript();
-            if (initialMigrationScript == null) {
-                return new MigraMongoStatus(
-                    "ERROR",
-                    "no last migration script found, and no initial migration script provided!");
-            }
-            executeInitialMigrationScript(initialMigrationScript);
-        }
-        return MigraMongoStatus.OK;
-    }
+	public MigraMongoStatus migrate() {
+		MigrationEntry lastMigrationApplied = getLastMigrationApplied();
+		if (lastMigrationApplied == null) {
+			final Object initialMigrationScript = getInitialMigrationScript();
+			if (initialMigrationScript == null) {
+				return new MigraMongoStatus("ERROR",
+						"no last migration script found, and no initial migration script provided!");
+			}
+			lastMigrationApplied = executeInitialMigrationScript(initialMigrationScript);
+		}
+		return MigraMongoStatus.OK;
+	}
 
-    private void executeInitialMigrationScript(Object initialMigrationScript) {
-        InitialMongoMigrationScript annotation = initialMigrationScript.getClass().getAnnotation(
-            InitialMongoMigrationScript.class);
-        if (annotation == null) {
-            throw new IllegalStateException("InitialMongoMigrationScript " +
-                initialMigrationScript +
-                " of class " +
-                initialMigrationScript.getClass().getName() +
-                " doesn't have an InitialMongoMigrationScript annotation!");
-        }
-        final Method method = getMigrationMethod(initialMigrationScript);
-        try {
-            executeMethod(method, initialMigrationScript);
-            setInitialVersionInMigrationCollection(annotation, null);
-        } catch (Exception e) {
-            setInitialVersionInMigrationCollection(annotation, e);
-        }
-    }
+	private MigrationEntry executeInitialMigrationScript(Object initialMigrationScript) {
+		InitialMongoMigrationScript annotation = initialMigrationScript.getClass().getAnnotation(
+				InitialMongoMigrationScript.class);
+		if (annotation == null) {
+			throw new IllegalStateException("InitialMongoMigrationScript " + initialMigrationScript + " of class "
+					+ initialMigrationScript.getClass().getName()
+					+ " doesn't have an InitialMongoMigrationScript annotation!");
+		}
+		final Method method = getMigrationMethod(initialMigrationScript);
+		try {
+			executeMethod(method, initialMigrationScript);
+			return setInitialVersionInMigrationCollection(annotation, null);
+		} catch (Exception e) {
+			return setInitialVersionInMigrationCollection(annotation, e);
+		}
+	}
 
-    private void setInitialVersionInMigrationCollection(InitialMongoMigrationScript annotation, Exception e) {
-        final Document document = new Document();
-        document.append("info", annotation.info());
-        document.append("module", annotation.module());
-        if (e == null) {
-            document.append("status", "OK");
-        } else {
-            document.append("status", "ERROR");
-            document.append("errorMessage", e.getMessage());
-        }
+	private MigrationEntry setInitialVersionInMigrationCollection(InitialMongoMigrationScript annotation, Exception e) {
+		final MigrationEntry migEntry = new MigrationEntry();
+		migEntry.module = annotation.module();
+		migEntry.status = "OK";
+		migEntry.fromVersion = annotation.version();
+		migEntry.toVersion = annotation.version();
+		migEntry.info = annotation.info();
 
-        getMigramongoCollection().insertOne(document);
+		final Document document = mapMigEntryToDocument(migEntry);
+		// TODO - where is the _id???
+		getMigramongoCollection().insertOne(document);
+		return migEntry;
 
-    }
+	}
 
-    private void executeMethod(Method method, Object obj) {
-        final Parameter[] parameters = method.getParameters();
-        if (parameters == null || parameters.length == 0) {
-            invoke(method, obj);
-        } else if (parameters.length == 1 && parameters[0].getClass().isAssignableFrom(MongoDatabase.class)) {
-            invoke(method, obj, database);
-        } else {
-            throw new IllegalStateException("method " +
-                method +
-                " in class " +
-                obj.getClass() +
-                " has either one parameter that is not of type MongoDatabase, or more than 1 parameter");
-        }
+	private void executeMethod(Method method, Object obj) {
+		final Parameter[] parameters = method.getParameters();
+		if (parameters == null || parameters.length == 0) {
+			invoke(method, obj);
+		} else if (parameters.length == 1 && parameters[0].getClass().isAssignableFrom(MongoDatabase.class)) {
+			invoke(method, obj, database);
+		} else {
+			throw new IllegalStateException("method " + method + " in class " + obj.getClass()
+					+ " has either one parameter that is not of type MongoDatabase, or more than 1 parameter");
+		}
 
-    }
+	}
 
-    private void invoke(Method method, Object obj, Object... args) {
-        try {
-            method.invoke(obj, args);
-        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-            throw new RuntimeException("Exception when invoking method " +
-                method +
-                " from MigrationScript " +
-                obj +
-                " of class " +
-                obj.getClass().getName(), e);
-        }
-    }
+	private void invoke(Method method, Object obj, Object... args) {
+		try {
+			method.invoke(obj, args);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			throw new RuntimeException("Exception when invoking method " + method + " from MigrationScript " + obj
+					+ " of class " + obj.getClass().getName(), e);
+		}
+	}
 
-    private Method getMigrationMethod(Object initialMigrationScript) {
-        final Method[] methods = initialMigrationScript.getClass().getDeclaredMethods();
-        final List<Method> publicMethods = Stream
-            .of(methods)
-            .filter(m -> Modifier.isPublic(m.getModifiers()))
-            .collect(Collectors.toList());
-        if (publicMethods.isEmpty()) {
-            throw new IllegalStateException("MigrationScript " +
-                initialMigrationScript +
-                " of class " +
-                initialMigrationScript.getClass().getName() +
-                " doesn't have any public method!");
-        }
-        if (publicMethods.size() > 1) {
-            throw new IllegalStateException("MigrationScript " +
-                initialMigrationScript +
-                " of class " +
-                initialMigrationScript.getClass().getName() +
-                " has " +
-                publicMethods.size() +
-                " public methods, but it should have only 1");
+	private Method getMigrationMethod(Object initialMigrationScript) {
+		final Method[] methods = initialMigrationScript.getClass().getDeclaredMethods();
+		final List<Method> publicMethods = Stream.of(methods).filter(m -> Modifier.isPublic(m.getModifiers()))
+				.collect(Collectors.toList());
+		if (publicMethods.isEmpty()) {
+			throw new IllegalStateException("MigrationScript " + initialMigrationScript + " of class "
+					+ initialMigrationScript.getClass().getName() + " doesn't have any public method!");
+		}
+		if (publicMethods.size() > 1) {
+			throw new IllegalStateException("MigrationScript " + initialMigrationScript + " of class "
+					+ initialMigrationScript.getClass().getName() + " has " + publicMethods.size()
+					+ " public methods, but it should have only 1");
 
-        }
-        return publicMethods.get(0);
-    }
+		}
+		return publicMethods.get(0);
+	}
 
-    public MongoCollection<Document> getMigramongoCollection() {
-        MongoCollection<Document> collection = this.database.getCollection(MIGRAMONGO_COLLECTION);
-        if (collection == null) {
-            this.database.createCollection(MIGRAMONGO_COLLECTION);
-            collection = this.database.getCollection(MIGRAMONGO_COLLECTION);
-        }
-        return collection;
-    }
+	public MongoCollection<Document> getMigramongoCollection() {
+		MongoCollection<Document> collection = this.database.getCollection(MIGRAMONGO_COLLECTION);
+		if (collection == null) {
+			this.database.createCollection(MIGRAMONGO_COLLECTION);
+			collection = this.database.getCollection(MIGRAMONGO_COLLECTION);
+		}
+		return collection;
+	}
 
-    public Document getLastMigrationApplied() {
-        return getMigramongoCollection().find().sort(new Document("createdAt", 1)).first();
-    }
+	public MigrationEntry getLastMigrationApplied() {
+		final Document doc = getMigramongoCollection().find().sort(new Document("createdAt", 1)).first();
+		return mapMigrationEntry(doc);
+	}
 
-    public Collection<Object> getMigrationScripts() {
-        return appContext.getBeansWithAnnotation(MongoMigrationScript.class).values();
-    }
+	private MigrationEntry mapMigrationEntry(Document doc) {
+		if (doc == null) {
+			return null;
+		}
+		final MigrationEntry migEntry = new MigrationEntry();
+		migEntry.fromVersion = doc.getString("fromVersion");
+		migEntry.toVersion = doc.getString("toVersion");
+		migEntry.createdAt = doc.getDate("createdAt");
+		migEntry.module = doc.getString("module");
+		migEntry.info = doc.getString("info");
+		migEntry.status = doc.getString("status");
+		migEntry.statusMessage = doc.getString("statusMessage");
+		return migEntry;
+	}
 
-    public Object getInitialMigrationScript() {
-        final Collection<Object> values = appContext.getBeansWithAnnotation(InitialMongoMigrationScript.class).values();
-        if (values.isEmpty()) {
-            return null;
-        }
-        if (values.size() > 1) {
-            throw new IllegalStateException("There cannot be more than one InitialMigrationScript!. Found " +
-                values.size() +
-                ": " +
-                values);
-        }
-        return values.iterator().next();
-    }
+	private Document mapMigEntryToDocument(MigrationEntry migEntry) {
+		final Document doc = new Document();
+		doc.put("fromVersion", migEntry.fromVersion);
+		doc.put("toVersion", migEntry.toVersion);
+		doc.put("createdAt", migEntry.createdAt);
+		doc.put("module", migEntry.module);
+		doc.put("info", migEntry.info);
+		doc.put("status", migEntry.status);
+		doc.put("statusMessage", migEntry.statusMessage);
+		return doc;
+	}
 
-    public void applyMigration(Object migrationObject) {
+	public Collection<Object> getMigrationScripts() {
+		return appContext.getBeansWithAnnotation(MongoMigrationScript.class).values();
+	}
 
-    }
+	public Object getInitialMigrationScript() {
+		final Collection<Object> values = appContext.getBeansWithAnnotation(InitialMongoMigrationScript.class).values();
+		if (values.isEmpty()) {
+			return null;
+		}
+		if (values.size() > 1) {
+			throw new IllegalStateException("There cannot be more than one InitialMigrationScript!. Found "
+					+ values.size() + ": " + values);
+		}
+		return values.iterator().next();
+	}
+
+	public void applyMigration(Object migrationObject) {
+
+	}
 
 }
---
+++ src/main/java/com/rinoto/migramongo/MigrationEntry.java
@@ -0,0 +1,15 @@
+package com.rinoto.migramongo;
+
+import java.util.Date;
+
+public class MigrationEntry {
+
+	public String module;
+	public String info;
+	public String fromVersion;
+	public String toVersion;
+	public Date createdAt;
+	public String status;
+	public String statusMessage;
+
+}
