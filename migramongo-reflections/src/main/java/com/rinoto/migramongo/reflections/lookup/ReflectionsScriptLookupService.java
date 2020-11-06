1e16a1ce24ba4384d3350be2d5f2e7de23407d1d
rubeninoto <ruben.inoto@redbull.com>
2020-11-06 19:32:46 +0100

    replacing reflections with classgraph

--- migramongo-reflections/src/main/java/com/rinoto/migramongo/reflections/lookup/ReflectionsScriptLookupService.java
+++ migramongo-reflections/src/main/java/com/rinoto/migramongo/reflections/lookup/ReflectionsScriptLookupService.java
@@ -3,47 +3,51 @@ package com.rinoto.migramongo.reflections.lookup;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.reflections.Reflections;
+import io.github.classgraph.*;
 
 import com.rinoto.migramongo.InitialMongoMigrationScript;
 import com.rinoto.migramongo.MongoMigrationScript;
 import com.rinoto.migramongo.lookup.ScriptLookupService;
 
 /**
- * Implementation of {@link com.rinoto.migramongo.lookup.ScriptLookupService} that uses the <a href="https://github.com/ronmamo/reflections">ronmamo reflections library</a> to find the classes implemeting the {@link com.rinoto.migramongo.MongoMigrationScript}s
- * 
+ * Implementation of {@link com.rinoto.migramongo.lookup.ScriptLookupService}
+ * that uses the <a href="https://github.com/classgraph/classgraph">ClassGraph reflections library</a> to find the classes implemeting the {@link com.rinoto.migramongo.MongoMigrationScript}s
+ *
  * @author rinoto
  */
 public class ReflectionsScriptLookupService implements ScriptLookupService {
 
-    final Reflections reflections;
+    final ClassGraph classGraph;
 
     public ReflectionsScriptLookupService(String basePackage) {
-        reflections = new Reflections(basePackage);
+        classGraph = new ClassGraph().enableAllInfo().acceptPackages(basePackage);
     }
 
     @Override
     public InitialMongoMigrationScript findInitialScript() {
 
-        final Set<Class<? extends InitialMongoMigrationScript>> initialMongoScriptClasses = reflections
-            .getSubTypesOf(InitialMongoMigrationScript.class);
-        if (initialMongoScriptClasses.isEmpty()) {
-            return null;
+        try (ScanResult scanResult = classGraph.scan()) {
+            final List<Class<? extends InitialMongoMigrationScript>> initialMongoScriptClasses =
+                    scanResult.getClassesImplementing(InitialMongoMigrationScript.class.getName())
+                            .stream()
+                            .map(routeClassInfo -> (Class<? extends InitialMongoMigrationScript>) routeClassInfo.loadClass())
+                            .collect(Collectors.toList());
+
+            if (initialMongoScriptClasses.isEmpty()) {
+                return null;
+            }
+            if (initialMongoScriptClasses.size() > 1) {
+                throw new IllegalStateException(
+                        "There cannot be more than one InitialMigrationScript!. Found " +
+                                initialMongoScriptClasses.size() +
+                                ": " +
+                                initialMongoScriptClasses);
+            }
+            final Class<? extends InitialMongoMigrationScript> initialMongoScriptClass = initialMongoScriptClasses.get(0);
+            return instantiate(initialMongoScriptClass);
         }
-        if (initialMongoScriptClasses.size() > 1) {
-            throw new IllegalStateException(
-                "There cannot be more than one InitialMigrationScript!. Found " +
-                    initialMongoScriptClasses.size() +
-                    ": " +
-                    initialMongoScriptClasses);
-        }
-        final Class<? extends InitialMongoMigrationScript> initialMongoScriptClass = initialMongoScriptClasses
-            .iterator()
-            .next();
-        return instantiate(initialMongoScriptClass);
     }
 
     private <T extends MongoMigrationScript> T instantiate(Class<T> clazz) {
@@ -58,16 +62,16 @@ public class ReflectionsScriptLookupService implements ScriptLookupService {
     @Override
     public Collection<MongoMigrationScript> findMongoScripts() {
 
-        final Set<Class<? extends MongoMigrationScript>> mongoScriptClasses = reflections
-            .getSubTypesOf(MongoMigrationScript.class);
-
-        final List<MongoMigrationScript> migScripts = mongoScriptClasses
-            .stream()
-            .filter(ms -> !InitialMongoMigrationScript.class.isAssignableFrom(ms))
-            .map(ms -> (MongoMigrationScript) instantiate(ms))
-            .collect(Collectors.toList());
-
-        return migScripts;
+        try (ScanResult scanResult = classGraph.scan()) {
+            final List<MongoMigrationScript> migScripts =
+                    scanResult.getClassesImplementing(MongoMigrationScript.class.getName())
+                            .stream()
+                            .map(routeClassInfo -> (Class<? extends MongoMigrationScript>) routeClassInfo.loadClass())
+                            .filter(ms -> !InitialMongoMigrationScript.class.isAssignableFrom(ms))
+                            .map(ms -> (MongoMigrationScript) instantiate(ms))
+                            .collect(Collectors.toList());
+            return migScripts;
+        }
     }
 
 }
